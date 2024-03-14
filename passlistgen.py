8c199318820282b42d4f8a56be34fedcc2da1ab5
yZ1337 <32521997+yZ1337@users.noreply.github.com>
2024-03-14 16:22:52 +0100

    Update passlistgen.py

--- passlistgen.py
+++ passlistgen.py
@@ -1,14 +1,4 @@
-import argparse
 import random
-import subprocess
-import sys
-
-try:
-    from colorama import Fore, Back, Style
-except ImportError:
-    print("colorama is not installed. Installing now...")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "colorama"])
-    from colorama import Fore, Back, Style
 
 def generate_suffixes():
     suffixes = ['01', '01!']
@@ -51,37 +41,3 @@ def modify_words(file_path, characters=None):
                 modified_lines.append(modified_word)
 
     return modified_lines
-
-def main():
-    print(f"{Fore.LIGHTMAGENTA_EX}{Style.DIM}#############################")
-    print(f"{Fore.LIGHTMAGENTA_EX}#      {Style.RESET_ALL}Created by: \033[4myZ\033[0m       {Style.DIM}{Fore.LIGHTMAGENTA_EX}#")
-    print(f"{Fore.LIGHTMAGENTA_EX}#############################{Style.RESET_ALL}\n")
-    parser = argparse.ArgumentParser(description=f'{Fore.LIGHTMAGENTA_EX}Modify words in a file by appending characters and save output.')
-    parser.add_argument('-w', '--wordlist', type=str, help='Path to the wordlist file')
-    parser.add_argument('-a', '--add', type=str, help='Characters to use, comma separated')
-    parser.add_argument('-o', '--output', type=str, help='File to save the modified words')
-
-    args = parser.parse_args()
-
-    if args.wordlist:
-        characters = None
-        if args.add:
-            characters = args.add.replace(' ', '').split(',')
-
-        modified_words = modify_words(args.wordlist, characters)
-        if args.output:
-            with open(args.output, 'w') as file:
-                count = 0
-                for word in modified_words:
-                    file.write(word + '\n')
-                    count += 1
-            print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Words generated: {Fore.LIGHTBLUE_EX}{count}{Style.RESET_ALL}")
-            print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Output saved to: {Fore.LIGHTBLUE_EX}{args.output}{Style.RESET_ALL}")
-        else:
-            for word in modified_words:
-                print(word)
-    else:
-        parser.print_help()
-
-if __name__ == "__main__":
-    main()
